ee4319ac3badc42d95cb6eafaeb3a995df5119b8
FrancisVuong <92435456+FrancisVuong@users.noreply.github.com>
2022-03-27 18:12:20 -0400

    update dependencies

--- Logistic_Regression_model.py
+++ Logistic_Regression_model.py
@@ -1,3 +1,12 @@
+# Import our dependencies
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+import pandas as pd
+import tensorflow as tf
+# Import checkpoint dependencies
+import os
+import pandas as pd 
+
 from sklearn.linear_model import LogisticRegression 
 from sklearn import metrics
 
--- RandomForest.py
+++ RandomForest.py
@@ -1,3 +1,22 @@
+# Import our dependencies
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from collections import Counter
+
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import confusion_matrix
+from imblearn.metrics import classification_report_imbalanced
+
+
+# Resample the training data with the BalancedRandomForestClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
+brf = BalancedRandomForestClassifier(n_estimators=100, random_state=1)
+brf.fit(X_train, y_train)
+
 # Build the model
  
 h2o.init()
