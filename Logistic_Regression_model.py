63f937afa2f083a1d18e5a2f221f3fa4a825cf2f
FrancisVuong <92435456+FrancisVuong@users.noreply.github.com>
2022-03-31 06:05:36 -0400

    Update Logistic_Regression_model.py

--- Logistic_Regression_model.py
+++ Logistic_Regression_model.py
@@ -13,3 +13,17 @@ from sklearn import metrics
 # create a simple, non-parameterized Logistic Regression model
 model = LogisticRegression(random_state=42)
 model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+from pprint import pprint
+# Look at parameters used by our current forest
+print('Parameters currently in use:\n')
+pprint(model.get_params())
+
+# create complex Logistic Regression with max_iter=131 
+log_model = LogisticRegression(max_iter=131, verbose=2, random_state=42)
+log_model.fit(x_train, y_train)
+y_pred_log = log_model.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred_log))
