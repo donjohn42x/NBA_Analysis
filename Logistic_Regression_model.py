021a57205d92eafb1557845204f520d27e88affe
FrancisVuong <92435456+FrancisVuong@users.noreply.github.com>
2022-03-31 17:46:31 -0400

    Update Logistic_Regression_model.py

--- Logistic_Regression_model.py
+++ Logistic_Regression_model.py
@@ -1,11 +1,26 @@
-# Import our dependencies
+
+# Model Training
+from sklearn.model_selection import train_test_split
+
+final_team_stats = pd.read_csv('Our dataframe here', index_col = 0)
+
+cols = [relevant coloums listed 1,2,3, etc.]
+final_team_stats.drop(final_team_stats.columns[cols],axis=1, inplace=True)
+
+features = final_team_stats.drop(columns = 'Label')
+label = final_team_stats['Label']
+
+x_train, x_test, y_train, y_test = train_test_split(features, label, test_size = 0.2)
+
+
+
+# Import our dependencies(not sure about these yet)
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 import pandas as pd
 import tensorflow as tf
-# Import checkpoint dependencies
 import os
-import pandas as pd 
+
 
 from sklearn.linear_model import LogisticRegression 
 from sklearn import metrics
